18c94b4ca6430bc718c2aa21043d648afaa68bd4
Jose Lemes <jose.lemes@pmu.net>
2018-08-27 15:09:45 -0300

    Ajustes no endpoint categorias/{id} e resource handler

--- src/main/java/com/joselemes/cursomc/services/CategoriaService.java
+++ src/main/java/com/joselemes/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.joselemes.cursomc.domain.Categoria;
 import com.joselemes.cursomc.repositories.CategoriaRepository;
+import com.joselemes.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -19,8 +20,25 @@ public class CategoriaService {
 //		return obj;
 //	}
 	
-	public Categoria buscar(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+	//Spring 2.2
+//	public Categoria buscar(Integer id) {
+//		Optional<Categoria> obj = repo.findById(id);
+//		return obj.orElse(null);
+//	}
+	
+	//Tratando erro
+//	public Categoria find(Integer id) {
+//		Categoria obj = repo.findOne(id);
+//		if (obj == null) {
+//		throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+//		+ ", Tipo: " + Categoria.class.getName());
+//		}
+//		return obj;
+//	}
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);		
+		String errMsg = "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName();
+		return obj.orElseThrow(() -> new ObjectNotFoundException(errMsg));
 	}
 }
